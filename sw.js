1db9d4559ae677214ba48799c594fa4d4730d398
pmugur68 <mugur.mircea.padure@gmail.com>
2026-09-09 05:56:47 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='prompt-vault-v3.5.3';
+const CACHE='prompt-vault-v3.5.4';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon-32.png'];
 
 self.addEventListener('install',event=>{
